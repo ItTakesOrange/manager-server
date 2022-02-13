1df439cf6018203db0cd58881d92798944da3068
bluefin07 <bluefin07@163.com>
2022-02-13 16:17:23 +0800

    feat: 菜单列表接口实现

--- routes/menus.js
+++ routes/menus.js
@@ -4,6 +4,38 @@ const util = require('../utils/util')
 
 router.prefix('/menu')
 
+// 菜单列表查询
+router.get('/list', async (ctx) => {
+  const { menuName, menuState } = ctx.request.query
+  const params = {}
+  if (menuName) params.menuName = menuName
+  if (menuState) params.menuState = menuState
+  const rootList = await Menu.find(params) || []
+  const permissionList = getTreeMenu(rootList, null, [])
+  ctx.body = util.success(permissionList, '成功')
+})
+
+function getTreeMenu(rootList, id, list) {
+  for(let i = 0; i < rootList.length; i++) {
+    const item = rootList[i]
+    if (String(item.parentId.slice().pop()) === String(id)) {
+      list.push(item._doc)
+    }
+  }
+  list.forEach(item => {
+    item.children = []
+    getTreeMenu(rootList, item._id, item.children)
+    if (item.children.length === 0) {
+      delete item.children
+    } else if (item.children.length > 0 && item.children[0].menuType === 2) {
+      // 快速区分菜单和按钮，用于后期做菜单按钮权限控制
+      item.action = item.children
+    }
+  })
+  return list
+}
+
+// 菜单编辑、删除、新增功能
 router.post('/operate', async (ctx) => {
   const { _id, action, ...params } = ctx.request.body
   try {
